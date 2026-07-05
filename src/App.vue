822e660350a4ef58ecf21969f73ed5e9ebe7741e
sergii13 <sergii.khilchuk13@gmail.com>
2026-07-05 11:55:22 +0300

    feat: add app layout with header and navigation drawer

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useNotificationStore } from './stores/notification'
+import AppHeader from './components/AppHeader.vue'
+import FilterPanel from './components/FilterPanel.vue'
+import EditorStage from './components/EditorStage.vue'
+
+const { messages } = storeToRefs(useNotificationStore())
+const drawer = ref(true)
+</script>
+
+<template>
+  <VApp>
+    <AppHeader @toggle-drawer="drawer = !drawer" />
+
+    <VNavigationDrawer v-model="drawer" width="320">
+      <FilterPanel />
+    </VNavigationDrawer>
+
+    <VMain class="h-screen">
+      <VContainer fluid class="pa-4 fill-height align-stretch d-flex">
+        <EditorStage />
+      </VContainer>
+    </VMain>
+
+    <VSnackbarQueue v-model="messages" location="top" :timeout="3000" />
+  </VApp>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const emit = defineEmits<{ (event: 'toggle-drawer'): void }>()
+</script>
+
+<template>
+  <VAppBar flat border="b" density="comfortable">
+    <template #prepend>
+      <VAppBarNavIcon @click="emit('toggle-drawer')" />
+    </template>
+
+    <VToolbarTitle class="text-center font-weight-bold text-h6">
+      LiveArt
+    </VToolbarTitle>
+
+    <template #append>
+      <div style="width: 48px" />
+    </template>
+  </VAppBar>
+</template>
